aebb3c52f4da7fd0ede5dba02674c3dd8ca9993b
Alexander Myers <alexander.j.myers@icloud.com>
2019-04-19 18:11:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function takeANumber(array, name){
   array.push(name)
-  return `Welcome ${name}, you are number ${array.length + 1} in line.`
+  return `Welcome, ${name}. You are number ${array.length + 1} in line.`
 }
 
 function nowServing(array){
